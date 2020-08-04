826fb96a9de5f22c9049cf1af89d476be39831a9
Sudipto Ghosh <68972214+BABIN2D@users.noreply.github.com>
2020-08-04 21:23:33 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,25 @@
+# def func1(a,b):
+#     avg = (a+b)/2
+#     return avg
+# print(x)
+# x = func1(12,43)
+# multiplication function
+
+# def func2(a,b,c,d):
+#     '''This function is used to multiply numbers'''
+#     mult =(a*b*c*d)
+#     return mult
+# x=func2(1,2,3,4)
+# print(x)
+
+# Factorial
+
+def function(a):
+    '''This function is used to get the factorial of a number'''
+    factorial = 1
+    while a >=1:
+
+        factorial = factorial*a
+        a-=1
+
+    return factorial
